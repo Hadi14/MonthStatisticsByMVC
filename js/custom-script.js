7acff84b31be5b8ec2a6e1c510ac926481cd6d14
Hadi14 <asusn43@yahoo.com>
2023-09-11 06:57:38 +0330

    correct rotate category__btn

--- js/custom-script.js
+++ js/custom-script.js
@@ -1,15 +1,9 @@
 
-// var showCatBtns = document.querySelector('#r-t');
-// var show = document.querySelector('.cat-rt');
-// showCatBtns.addEventListener("click", function () {
-//     showCatBtns.classList.toggle('rotated');
-//     // alert("sdf");
-// });
 
 var show = document.querySelectorAll('.cat-rt');
 console.log(show);
 show.forEach(element => {
     element.addEventListener('click', (e) => {
-        console.log(e);
+        e.target.childNodes[3].classList.toggle('rotated');
     })
 });
