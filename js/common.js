3027a2a2aacfefb69ec5801b0a421da25c4d4ea7
Hadi14 <asusn43@yahoo.com>
2023-10-07 11:26:45 +0330

    optim modals

--- js/common.js
+++ js/common.js
@@ -36,3 +36,13 @@ $(document).ready(function () {
         },
     });
 });
+
+$('.modal.fade').on('hidden.bs.modal', function (e) {
+    $(this)
+        .find("input,textarea,select")
+        .val('')
+        .end()
+        .find("input[type=checkbox], input[type=radio]")
+        .prop("checked", "")
+        .end();
+})
