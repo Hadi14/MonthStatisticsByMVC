5d7da7e7013b76bc34cc607b3f02f2645a8603c1
Hadi14 <asusn43@yahoo.com>
2023-10-18 11:24:12 +0330

    rgdfg

--- mvc/controller/upload.php
+++ mvc/controller/upload.php
@@ -1,27 +1,21 @@
 <? class uploadController
 {
+    static $count = 1;
     function uploadPdf()
     {
 
         $target_dir = "uploads/";
         // dump($_FILES);
+        $pureName = basename($_FILES["fileToUpload"]["name"]);
+        $sepName = explode('.', $pureName);
+        dump($sepName[0] . self::$count++ . '.pdf');
+        $fileName = $sepName[0] . self::$count++ . '.pdf';
 
+        $target_file = $target_dir . $fileName;
+        dump($target_file);
 
-        $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
         $uploadOk = 1;
         $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-        // // Check if image file is a actual image or fake image
-        // if (isset($_POST["submit"])) {
-        //     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        //     if ($check !== false) {
-        //         echo "File is an image - " . $check["mime"] . ".";
-        //         $uploadOk = 1;
-        //     } else {
-        //         echo "File is not an image.";
-        //         $uploadOk = 0;
-        //     }
-        // }
 
         // Check if file already exists
         if (file_exists($target_file)) {
@@ -29,6 +23,16 @@
             $uploadOk = 0;
         }
 
+        if (!($_FILES["fileToUpload"]["name"])) {
+            echo "Sorry, No file selected. Please Select a file.";
+            $uploadOk = 0;
+        }
+
+
+
+
+
+        exit;
         // Check file size
         if ($_FILES["fileToUpload"]["size"] > 500000) {
             echo "Sorry, your file is too large.";
