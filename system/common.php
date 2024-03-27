31aac060f4f5da3842d7fce010fdc7158af6ea24
Hadi14 <asusn43@yahoo.com>
2024-03-27 13:56:17 +0330

    اضافه کردن تابع صحت سنجی کد ملی

--- system/common.php
+++ system/common.php
@@ -63,3 +63,24 @@ function getLastInsert($tbl, $yrFieldName, $monFieldName, $yer, $mnth)
 //         exit;
 //     }
 // }
+function checkMeliCode($meli)
+{
+    $cDigitLast = substr($meli, strlen($meli) - 1);
+    $fMeli = strval(intval($meli));
+    if ((str_split($fMeli))[0] == "0" && !(8 <= strlen($fMeli)  && strlen($fMeli) < 10)) return false;
+    $nineLeftDigits = substr($meli, 0, strlen($meli) - 1);
+    $positionNumber = 10;
+    $result = 0;
+    foreach (str_split($nineLeftDigits) as $chr) {
+        $digit = intval($chr);
+        $result += $digit * $positionNumber;
+        $positionNumber--;
+    }
+    $remain = $result % 11;
+    $controllerNumber = $remain;
+    if (2 <= $remain) {
+        $controllerNumber = 11 - $remain;
+    }
+    return $cDigitLast == $controllerNumber;
+    //   echo checkMeliCode($meli) ? 'نا معتبر' :  معتبر';
+}
